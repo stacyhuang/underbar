8d74200f28dd6299b97f668971281deba66a854e
Stacy <syhhuang88@gmail.com>
2015-03-24 16:51:40 -0700

    Implemented the _.reject function

--- src/underbar.js
+++ src/underbar.js
@@ -90,6 +90,11 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var passedArray = _.filter(collection, test);
+    var newArray = _.filter(collection, function(num){
+    	return _.indexOf(passedArray, num)<0;
+    });
+    return newArray;
   };
 
   // Produce a duplicate-free version of the array.
