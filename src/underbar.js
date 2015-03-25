c2f0f6300ac008fa233065c428c0ce0203b53f3d
Stacy <syhhuang88@gmail.com>
2015-03-24 22:31:11 -0700

    Implemented the _.map function

--- src/underbar.js
+++ src/underbar.js
@@ -111,8 +111,17 @@
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
-
+    var newCollection = [];
+    if(Array.isArray(collection)){
+    	for(var i=0; i<collection.length; i++){
+    		newCollection.push(iterator(collection[i], i, collection));
+    	}
+    }else{
+    	for(var key in collection){
+    		newCollection.push(iterator(collection[key], key, collection));
+    	}
+    }
+    return newCollection;
   };
 
   /*
